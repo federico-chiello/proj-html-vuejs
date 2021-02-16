21ba8d49a4d664bfcd6ddaba2b0b782038caa0f9
Fede <chiellofederico@gmail.com>
2021-02-16 19:23:48 +0100

    footer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 var app = new Vue({
   el: '#avada',
   data:{
-    menu: ['Home', 'Who We Are', 'What We Do', 'Where We Work', 'Careers Apply', 'News', 'Get in touch now']
+    menu: ['Home', 'Who We Are', 'What We Do', 'Where We Work', 'Careers Apply', 'News', 'Get in touch now'],
+    menuFooter: ['Home', 'Who We Are', 'What We Do', 'Where We Work', 'Careers', 'News', 'Get in touch now']
   }
 });
