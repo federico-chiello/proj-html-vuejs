5d2b98bd64466b46838b518eb4d0e5ca96ef6222
Fede <chiellofederico@gmail.com>
2021-02-17 16:09:47 +0100

    fine slider

--- js/script.js
+++ js/script.js
@@ -56,29 +56,30 @@ var app = new Vue({
         image: 'seo/images/testimonials-2.jpg',
         review: 'Ability proceeds from a fusion of skills, knowledge, understanding and imagination, consolidated by experience.',
         name: 'Luis Desalvo',
-        role: 'CREO TECH',
-        visible: 'show'
+        role: 'CREO TECH'
       },
       {
         image: 'seo/images/testimonials-1.jpg',
         review: 'Avada allowed me to carry out advertising campaigns without any difficulty.',
         name: 'Rachel Stuart',
-        role: 'DIGITAL MARKETER',
-        visible: 'show'
+        role: 'DIGITAL MARKETER'
       }
     ]
+  },
+  stopInterval: '',
+  created(){
+    this.stopInterval = setInterval(this.nextClient,4000);
   },
   methods: {
     nextClient(){
       (this.counter < (this.clients.length - 1)) ? this.counter++ : this.counter = 0;
-      console.log(this.counter);
     },
     previousClient(){
       (this.counter < 1) ? this.counter = (this.clients.length -1) : this.counter--;
-      console.log(this.counter);
     },
     clickCircle(index){
       this.counter = index;
+      clearInterval(this.stopInterval);
     }
   }
 });
